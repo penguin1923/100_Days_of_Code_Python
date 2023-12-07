923c45b729657fa4834b28346710c474f70baf07
Don <64542224+penguin1923@users.noreply.github.com>
2023-12-07 10:56:05 -0500

    added newline after questions

--- 1 Band Name Generator/generator.py
+++ 1 Band Name Generator/generator.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator")
-city = input("What is the name of the city that you grew up in? ")
-pet = input("What's the name of your pet? ")
+city = input("What is the name of the city that you grew up in?\n")
+pet = input("What's the name of your pet?\n")
 print('Your band name could be ' + city,pet)
