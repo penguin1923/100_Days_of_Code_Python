16f79d41b4eae85ec9806bece68ccfa65069eb41
Don <64542224+penguin1923@users.noreply.github.com>
2023-12-11 13:07:08 -0500

    better endings for failure

--- 3 Choose Your Own Adventure Game/game.py
+++ 3 Choose Your Own Adventure Game/game.py
@@ -23,16 +23,24 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 choice = input("You come to the end of a hallway and can either go 'left' or 'right'. Pick one now.\n")
-if choice == "left":
+if choice.lower() == "left":
     choice = input("You now are at a river. Should you 'swim' or 'wait'?\n")
-    if choice == "wait":
+    if choice.lower() == "wait":
         choice = input("Now you are in front of three doors and you need to pick one. Should you go in the 'red' on the 'blue' one or the 'yellow' one?\n")
-        if choice == "yellow":
-            print("You Win!!!")
+        if choice.lower() == "yellow":
+            print("You Win!!! The room is full of treasure and there is an uber waiting outside to carry it all home.")
+        elif choice.lower() == "blue":
+            print("You immediately regret this decision as a million fire ants attack you cleaning you all the way to the bone")
+        elif choice.lower() == "red":
+            print("The door was heavy as you pushed it open. Unfortunately due to its weight you were unable to close it before the pirates saw you and captured you.")
         else:
             print("Game Over")
+    elif choice.lower() == "swim":
+        print("This was a terrible choice as the undertow dragged you down never to be seen again.")
     else:
         print("Game Over")
+elif choice.lower() == "right":
+    print("Right was not right!! You walked to the end of the next hallway to find out that you have been trapped in a never-ending maze")
 else:
     print("Game Over")
     
