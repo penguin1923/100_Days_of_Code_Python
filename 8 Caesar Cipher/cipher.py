e339e7978f93237c81f1e9cd468688e669aaec99
Don <64542224+penguin1923@users.noreply.github.com>
2024-01-31 10:44:29 -0500

    step 1 of cypher working

---
+++ 8 Caesar Cipher/cipher.py
@@ -0,0 +1,22 @@
+import cipher_alphabet
+import cipher_art
+
+print(cipher_art.logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+text_index = []
+new_word = []
+
+def encrypt(texts,shifts):
+    for letter in texts:
+        text_index.append(letter)
+
+    for input in text_index:
+        index1 = cipher_alphabet.alphabet.index(input)
+        index2 = cipher_alphabet.alphabet[index1 + shifts]
+        new_word.append(index2)
+encrypt(text,shift)
+
+joined_new_word = "".join(new_word)
+print(f"The encoded text is {joined_new_word}")
--- 8 Caesar Cipher/practice.py
+++ 8 Caesar Cipher/practice.py
@@ -1,13 +1,27 @@
 #name=input("What is your name? ")
 
+"""day 8 practice materials"""
+
 def greet():
+    """base function work"""
     print("Good Morning")
     print("This day seems a little bit cold")
     print("Make sure that you dress warmly to leave the house")
 
 def greet_with_name(name):
+    """added one parameter"""
     print(f"Good Morning {name}")
     print("This day seems a little bit cold")
     print("Make sure that you dress warmly to leave the house")
 
-greet_with_name("Don")
+#greet_with_name("Don")
+
+def greet_with_multiple_parameters(name,location):
+    """added multiple parameters"""
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+#positional arguments
+# greet_with_multiple_parameters("Don","PA")
+# keyword arguments
+greet_with_multiple_parameters(location="England",name="April")
