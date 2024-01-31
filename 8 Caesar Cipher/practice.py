533c21f6f781184fedb796ef4ff9558c286b8e37
Don <64542224+penguin1923@users.noreply.github.com>
2024-01-31 08:39:53 -0500

    working with perameters

---
+++ 8 Caesar Cipher/practice.py
@@ -0,0 +1,13 @@
+#name=input("What is your name? ")
+
+def greet():
+    print("Good Morning")
+    print("This day seems a little bit cold")
+    print("Make sure that you dress warmly to leave the house")
+
+def greet_with_name(name):
+    print(f"Good Morning {name}")
+    print("This day seems a little bit cold")
+    print("Make sure that you dress warmly to leave the house")
+
+greet_with_name("Don")
