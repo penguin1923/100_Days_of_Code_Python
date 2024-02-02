910a2aa18189aab6f04ae7e035891f046cadc63a
Don <64542224+penguin1923@users.noreply.github.com>
2024-02-02 11:04:55 -0500

    working cipher

--- 8 Caesar Cipher/cipher.py
+++ 8 Caesar Cipher/cipher.py
@@ -12,8 +12,8 @@ def encrypt(texts,shifts):
     for letter in texts:
         text_index.append(letter)
 
-    for input in text_index:
-        index1 = cipher_alphabet.alphabet.index(input)
+    for letter in text_index:
+        index1 = cipher_alphabet.alphabet.index(letter)
 
         if (index1 + shifts)<=25:
             index2 = cipher_alphabet.alphabet[index1 + shifts]
@@ -21,7 +21,37 @@ def encrypt(texts,shifts):
         else:
             index2 = cipher_alphabet.alphabet[(index1 + shifts)-26]
             new_word.append(index2)
-encrypt(texts=text,shifts=shift)
 
-joined_new_word = "".join(new_word)
-print(f"The encoded text is {joined_new_word}")
+def decrypt(texts,shifts):
+    for letter in texts:
+        text_index.append(letter)
+
+    for letter in text_index:
+        index1 = cipher_alphabet.alphabet.index(letter)
+        # this isn't technically needed as python can accept negative index locations
+        if (index1 - shifts) <= 25:
+            index2 = cipher_alphabet.alphabet[index1 - shifts]
+            new_word.append(index2)
+        else:
+            index_location = (index1 - shifts) + 26
+            index2 = cipher_alphabet.alphabet[index_location]
+            new_word.append(index2)
+
+
+
+condition = True
+
+while condition:
+    if direction == "encode":
+        encrypt(texts=text,shifts=shift)
+        joined_new_word = "".join(new_word)
+        print(f"The encoded text is {joined_new_word}")
+        condition = False
+    elif direction == "decode":
+        decrypt(texts=text,shifts=shift)
+        joined_new_word = "".join(new_word)
+        print(f"The decoded text is {joined_new_word}")
+        condition = False
+    else:
+        print("That is not a valid option please try again")
+        condition = False
