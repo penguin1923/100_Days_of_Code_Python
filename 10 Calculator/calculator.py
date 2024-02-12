d7bb5f4c82950a73a87d574044b296260c17f26b
Don <64542224+penguin1923@users.noreply.github.com>
2024-02-12 09:17:11 -0500

    Finished calculator

--- 10 Calculator/calculator.py
+++ 10 Calculator/calculator.py
@@ -1,6 +1,8 @@
 """Day 10 Final Project: Text based calculator"""
 import calculator_art
 
+print(calculator_art.logo)
+
 def add(n1,n2):
     """adds n1 to n2"""
     return n1 + n2
@@ -19,15 +21,35 @@ def divide(n1,n2):
 
 operations = {"+":add,"-":subtract,"*":multiply,"/":divide}
 
-num1 = int(input("What is the first number? "))
-num2 = int(input("What is the second number? "))
+def calculator():
+    num1 = float(input("What is the first number? "))
 
-for symbol in operations:
-    print(symbol)
+    end_of_calculation = False
 
-operation_symbol = input("Pick an operation to calculate: ")
+    while not end_of_calculation:
+        for symbol in operations:
+            print(symbol)
+        valid_operation = False
+        while not valid_operation:
+            operation_symbol = input("Pick an operation to calculate: ")
+            if operation_symbol in operations.keys():
+                valid_operation = True
 
-answer = operations[operation_symbol](num1,num2)
+        num2 = float(input("What is the next number? "))
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+        answer = operations[operation_symbol](num1,num2)
 
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        would_continue = input(f"Type 'y' to continue calculating with {answer}, type 'n' to to start a new calculation. ")
+
+        if would_continue == 'y':
+            num1 = answer
+            continue
+        elif would_continue == 'n':
+            end_of_calculation = True
+            calculator()
+        else:
+            end_of_calculation = True
+
+calculator()
