60c69fdee08d8c58d138053a861feb85b6b0a947
Don <64542224+penguin1923@users.noreply.github.com>
2024-02-12 16:27:10 -0500

    working blackjack game

--- 11 Blackjack -- Capstone/blackjack.py
+++ 11 Blackjack -- Capstone/blackjack.py
@@ -1,3 +1,78 @@
+import random
 import blackjack_art
 
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+player_cards = []
+dealer_cards = []
+random_card = -1
+player_score = -1
 print(blackjack_art.logo)
+
+
+def deal_card():
+    random_card = random.choice(cards)
+    return random_card
+
+def determine_score(a):
+    score = sum(a)
+    if score > 21:
+        if 11 in player_cards:
+            score -= 10
+            return score
+        elif 11 not in player_cards:
+            return score
+    else:
+        return score
+
+def determine_winner():
+    player_score=determine_score(player_cards)
+    dealer_score=determine_score(dealer_cards)
+    if player_score>21:
+        print(f"You Busted with a score of {player_score}")
+    elif player_score>dealer_score:
+        print(f"You beat the dealer!!\nYou had a score of {player_score}\nThe dealer had a score of {dealer_score}")
+    else:
+        print(f"The dealer beat you.\nYou had a score of {player_score}\nThe dealer had a score of {dealer_score}")
+
+def blackjack():
+    player_cards.append(deal_card())
+    player_cards.append(deal_card())
+    dealer_cards.append(deal_card())
+    dealer_cards.append(deal_card())
+    print(f"Your cards are {player_cards[0]} and {player_cards[1]} for a score of {determine_score(player_cards)}")
+    print(f"The dealer is showing {dealer_cards[0]}")
+    
+    did_hit = True
+
+    while did_hit:   
+        if determine_score(dealer_cards) == 21:
+            print("The dealer was dealt Blackjack\n You Loose!!")
+            did_hit=False
+            break
+        elif determine_score(player_cards) == 21:
+            print("BlackJack !!!")
+            did_hit = False
+            break
+        hit = input("Would you like to get another card? 'y' or 'n' ")
+        if hit == 'y':
+            player_cards.append(deal_card())
+            print(f"Your new card is {player_cards[-1]} for a score of {determine_score(player_cards)}")
+            if determine_score(player_cards)>21:
+                determine_winner()
+                did_hit = False
+        else:
+            determine_winner()
+            did_hit= False
+
+
+# play_again = True
+# again = input("Would you like to play again? 'y' or 'n' ")
+
+# while play_again:
+#     if again == 'y':
+#         blackjack()
+#     else:
+#         play_again = False
+
+blackjack()
