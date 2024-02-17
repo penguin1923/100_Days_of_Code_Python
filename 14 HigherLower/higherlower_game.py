8accaf3e964030500641f877d90cf752ba5f97ae
Don <64542224+penguin1923@users.noreply.github.com>
2024-02-17 10:15:04 -0500

    have two while loops using the same logic need to fix them

--- 14 HigherLower/higherlower_game.py
+++ 14 HigherLower/higherlower_game.py
@@ -13,20 +13,38 @@ def get_random_account():
 def make_data_readable():
     return
 
+def game_compare(guess, a_follower,b_follower):
+    
+    if guess == "A" and a_follower>b_follower:
+        return True
+    elif guess == "B" and b_follower>a_follower:
+        return True
+    else:
+        return False
+
 def game():
     clear_screen()
     print(higherlower_art.logo)
     score = 0
     random_a = get_random_account()
+    a_format=f"{random_a['name']}, {random_a['description']}, from {random_a['country']}"
     random_b = get_random_account()
+    b_format=f"{random_b['name']}, {random_b['description']}, from {random_b['country']}"
     game_should_continue = True
 
     while game_should_continue:
-        print("Hello")
     # need to format random_a before printing
-        print(random_a["name"])
+        print("Compare A: "+a_format)
+        print(higherlower_art.vs)
+        print("Against B: "+b_format)
+        guess=input("Who has more followers? Type 'A' or 'B'").upper()
 
         game_should_continue = False
+        game_should_continue = game_compare(guess,random_a['follower_count'],random_b['follower_count'])
+        while game_should_continue:
+            score+=
+            random_a = random_b
+            random_b = get_random_account()
 
 
 
